1e11c5aa8dfaec65d4424d39c277f11688a60146
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-07-20 16:33:31 -0500

    serving index.html

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const getCachedSensorReadings = require('./getCachedSensorReadings');
 
@@ -6,9 +7,13 @@ app.get('/temperature', function(req, res) {
     res.send('<strong>' + getCachedSensorReadings.getTemperature().toFixed(1) + '</strong>' + '°C') 
     });
 
-    app.get('/humidity', function(req, res) {
-        res.send('<strong>' + getCachedSensorReadings.getHumidity().toFixed(1) + '</strong>' + '%') 
-        });
+ app.get('/humidity', function(req, res) {
+    res.send('<strong>' + getCachedSensorReadings.getHumidity().toFixed(1) + '</strong>' + '%') 
+    });
+
+app.get('/publlic', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
 
 app.listen(3000, function() {
     console.log('Server listening on port 3000');
