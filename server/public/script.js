34aa03107e072b0bf4a0efae28f361c1ec37a136
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-07-20 18:02:20 -0500

    Implemented ChartJS

--- server/public/script.js
+++ server/public/script.js
@@ -27,3 +27,25 @@ setInterval(() => {
     fetchTemperature();
     fetchHumidity();
 }, 2000)
+
+const tempertureCanvasCtx =
+document.getElementById('temperature-chart').getContext('2d')
+
+const temperatureChart = new CharacterData(tempertureCanvasCtx,
+    {
+        type: 'line',
+        data: {
+            labels: ['10:30', '10:31', '10:32', '10:33'],
+            datasets: [{
+                data: [12, 19, 23, 17],
+                backgroundColor: 'rgba(255, 205, 210, 0.5)'
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    })
