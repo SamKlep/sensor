3a60fda87035d96f38f98022f578d6e34fcd6837
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-07-20 10:02:32 -0500

    cached sensor readings and implemented into index.js

--- server/index.js
+++ server/index.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const app = express();
-const getSensorReadings = require('./getSensorReadings');
-const sensor = require('node-dht-sensor');
+const getCachedSensorReadings = require('./getCachedSensorReadings');
 
 app.get('/temperature', function(req, res) {
-    getSensorReadings((err, temperature, humidity) {
-        if(!err) {
-            res.send(temperature.toFixed(1) + '°C');
-        }
-        
+    res.send(getCachedSensorReadings.getTemperature().toFixed(1) + '°C') 
     });
-});
 
-app.get('/humidity', function(req, res) {
-    getSensorReadings((err, temperature, humidity) {
-        if(!err) {
-            res.send(humidity.toFixed(1) + '°C');
-        }
-        
-    });
-});
+    app.get('/humidity', function(req, res) {
+        res.send(getCachedSensorReadings.getHumidity().toFixed(1) + '%') 
+        });
 
 app.listen(3000, function() {
     console.log('Server listening on port 3000');
