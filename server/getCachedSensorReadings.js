b9bdb154943ab19afb2c04898597fdc440bc8020
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-07-21 11:17:16 -0500

    sql test 3

--- server/getCachedSensorReadings.js
+++ server/getCachedSensorReadings.js
@@ -12,8 +12,8 @@ setInterval(() => {
         if(err) {
             return console.error(err)
         }
-        databaseOperations.insertReading('temperature', temperature)
-        databaseOperations.insertReading('humidity', humidity)
+        databaseOperations.insertReading('dhtreadings', temperature)
+        databaseOperations.insertReading('dhtreadings', humidity)
         cache.temperature = temperature;
         cache.humidity = humidity;
     })
