7231bf319f7fd7d96996d7c664e62fa399c7a770
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-07-21 07:56:47 -0500

    mongoDB undone

--- server/index.js
+++ server/index.js
@@ -2,18 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const getCachedSensorReadings = require('./getCachedSensorReadings');
-const colors = require('colors');
-const connectDB = require('./config/db');
-const dotenv = require('dotenv');
+
 // const databaseOperations = require('./databaseOperations');
-
-dotenv.config({ path: './config/config.env' });
-
-// Connect to database
-connectDB();
-
-// Body parser
-app.use(express.json());
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
@@ -51,14 +41,6 @@ app.get('/temperature', function(req, res) {
 // })
 // })
 
-const server = app.listen(
-    PORT,
-    console.log(`Server running on port ${PORT}`.yellow.bold)
-  );
-
-  // Handle unhandled 
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message.red}`);
-    // Close server and exit process
-    server.close(() =>process.exit(1));
+app.listen(3000, function() {
+    console.log('Server listening on port 3000');
 });
