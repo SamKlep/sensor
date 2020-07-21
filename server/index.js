5fba70d6c5c2743c935869fadc8b01c4ea92eb1c
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-07-21 11:33:18 -0500

    removed sql3

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// const getCachedSensorReadings = require('./getCachedSensorReadings');
+const getCachedSensorReadings = require('./getCachedSensorReadings');
 
 // const databaseOperations = require('./databaseOperations');
 
