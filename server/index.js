1e50c81d9c9e4e261fbcddfea9b76f82f43b2588
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-07-21 21:37:39 -0500

    sql testing

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const getCachedSensorReadings = require('./getCachedSensorReadings');
 
-// const databaseOperations = require('./databaseOperations');
+const databaseOperations = require('./databaseOperations');
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
